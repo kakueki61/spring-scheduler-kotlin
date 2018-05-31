2850d1bf88fedf1dd81d9f71f14a7f99d5780815
TakuyaKodama <t.kodama61@gmail.com>
2018-06-01 01:12:02 +0900

    initial commit

---
+++ src/test/kotlin/net/kakueki61/springscheduler/SpringSchedulerApplicationTests.kt
@@ -0,0 +1,16 @@
+package net.kakueki61.springscheduler
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringSchedulerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
