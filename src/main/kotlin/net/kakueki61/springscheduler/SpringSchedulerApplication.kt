db181051dd5f6c3feff9b99f46f0dca4bd5d756b
Takuya Kodama <takuyakodama@r.recruit.co.jp>
2018-06-10 22:34:33 +0900

    set up accounts table

--- src/main/kotlin/net/kakueki61/springscheduler/SpringSchedulerApplication.kt
+++ src/main/kotlin/net/kakueki61/springscheduler/SpringSchedulerApplication.kt
@@ -1,10 +1,24 @@
 package net.kakueki61.springscheduler
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootApplication
-class SpringSchedulerApplication
+class SpringSchedulerApplication {
+    @Bean
+    fun commandLineRunner(jdbcTemplate: JdbcTemplate) = CommandLineRunner {
+        jdbcTemplate.execute("""CREATE TABLE IF NOT EXISTS accounts (
+            id          BIGINT          PRIMARY KEY AUTO_INCREMENT,
+            user_name   VARCHAR(100)    NOT NULL,
+            password    VARCHAR(100)    NOT NULL,
+            role        VARCHAR(8)      NOT NULL,
+            enabled     BOOLEAN         NOT NULL    DEFAULT FALSE
+        )""")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringSchedulerApplication>(*args)
